fab0b5625617916e4676f22c38e2ebc3d7b94c21
RonnyWaskow <ronny.waskow@googlemail.com>
2026-08-26 22:37:07 +0200

    Add files via upload

---
+++ 9-2_Native SegWit (BIP84) für bc1q-Adressen.cs
@@ -0,0 +1,35 @@
+using System;
+using NBitcoin;
+
+// Mnemonic aus dem Testvektor, hier als abandon-Beispiel
+string worte =
+    "abandon abandon abandon abandon abandon abandon " +
+    "abandon abandon abandon abandon abandon about";
+Mnemonic bestehend = new Mnemonic(worte, Wordlist.English);
+
+// Seed aus dem Mnemonic ableiten (optionale Passphrase, hier leer)
+byte[] seed = bestehend.DeriveSeed();
+
+Console.WriteLine("Seed (64 Byte):");
+Console.WriteLine(Convert.ToHexString(seed).ToLower());
+
+// Master Key aus dem Mnemonic ableiten
+ExtKey masterKey = bestehend.DeriveExtKey();
+ 
+Console.WriteLine("Master Private Key:");
+Console.WriteLine(masterKey.PrivateKey.ToHex());
+ 
+// Als xprv (Base58Check, beginnt mit 'xprv')
+Console.WriteLine("xprv: " + masterKey.GetWif(Network.Main));
+
+// BIP84-Pfad für die externen Adressen des ersten Kontos
+var pfad84 = KeyPath.Parse("m/84'/0'/0'/0");
+
+for (int i = 0; i < 3; i++)
+{
+    ExtKey kindKey = masterKey.Derive(pfad84).Derive((uint)i);
+    BitcoinAddress adresse = kindKey.PrivateKey.GetAddress(
+        ScriptPubKeyType.Segwit, Network.Main);
+
+    Console.WriteLine($"Adresse {i}: {adresse}");
+}
